19683bd9d30a08b4e2da90192c15f57d2f03f438
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-12-13 17:23:28 +0200

    loader-fix

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -9,7 +9,7 @@ export default function SharedLayout() {
   return (
     <>
       <Header />
-      <Suspense fallback={Loader}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
       <Toaster toastOptions={toastOptions} />
