eb8dc39117fd13c198f4c1a01b454a59046f0227
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-12-17 21:23:11 +0200

    Started adding files for tablet

--- src/pages/SettingsPage/SettingsPage.styled.js
+++ src/pages/SettingsPage/SettingsPage.styled.js
@@ -3,8 +3,21 @@ import styled from 'styled-components';
 export const SettingsPageTitle = styled.h1`
   margin: 16px auto;
   font-size: 24px;
+
+  @media screen and (min-width: 834px) {
+    margin-top: 24px;
+    margin-bottom: 20px;
+    font-size: 30px;
+    font-weight: 500;
+    line-height: 1.2;
+  }
 `;
 
 export const SettingsPageImg = styled.img`
   margin-bottom: 24px;
+
+  @media screen and (min-width: 834px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
