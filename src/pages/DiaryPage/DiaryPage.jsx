e7f70a0dd4e1f63f9afb126abd2804d6f43eb9ba
titova-polina <112263937+titova-polina@users.noreply.github.com>
2023-12-19 01:42:23 +0200

    made the structure and styles

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,5 +1,228 @@
-import React from 'react';
+import {
+  DiarySection,
+  GoBackBtn,
+  GoBackWrapper,
+  ListItem,
+  List,
+  StyledBreakfastIcon,
+  StyledGoBackIcon,
+  MealWrapper,
+  NutririonList,
+  StyledLunchIcon,
+  StyledDinnerIcon,
+  StyledSnackIcon,
+  RecordMealWrapper,
+  RecordBtnLabel,
+  RecordMealIcon,
+  RecordBtn,
+  MealTitleWrapper,
+} from './DiaryPage.styled';
 
 export default function DiaryPage() {
-  return <div>DiaryPage</div>;
+  return (
+    <DiarySection>
+      <GoBackWrapper>
+        <GoBackBtn>
+          <StyledGoBackIcon />
+          <h2>Diary</h2>
+        </GoBackBtn>
+      </GoBackWrapper>
+      <MealWrapper>
+        <MealTitleWrapper>
+          <StyledBreakfastIcon />
+          <h3>Breakfast</h3>
+        </MealTitleWrapper>
+        <NutririonList>
+          <li>
+            Carbonohidrates:<span></span>
+          </li>
+          <li>
+            Protein:<span></span>
+          </li>
+          <li>
+            Fat:<span></span>
+          </li>
+        </NutririonList>
+        <RecordMealWrapper>
+          <List>
+            <ListItem>
+              <p>1</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>2</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>3</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>4</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+          </List>
+        </RecordMealWrapper>
+      </MealWrapper>
+      <MealWrapper>
+        <MealTitleWrapper>
+          <StyledLunchIcon />
+          <h3>Lunch</h3>
+        </MealTitleWrapper>
+        <NutririonList>
+          <li>
+            Carbonohidrates:<span></span>
+          </li>
+          <li>
+            Protein:<span></span>
+          </li>
+          <li>
+            Fat:<span></span>
+          </li>
+        </NutririonList>
+        <RecordMealWrapper>
+          <List>
+            <ListItem>
+              <p>1</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>2</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>3</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>4</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+          </List>
+        </RecordMealWrapper>
+      </MealWrapper>
+      <MealWrapper>
+        <MealTitleWrapper>
+          <StyledDinnerIcon />
+          <h3>Dinner</h3>
+        </MealTitleWrapper>
+        <NutririonList>
+          <li>
+            Carbonohidrates:<span></span>
+          </li>
+          <li>
+            Protein:<span></span>
+          </li>
+          <li>
+            Fat:<span></span>
+          </li>
+        </NutririonList>
+        <RecordMealWrapper>
+          <List>
+            <ListItem>
+              <p>1</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>2</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>3</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>4</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+          </List>
+        </RecordMealWrapper>
+      </MealWrapper>
+      <MealWrapper>
+        <MealTitleWrapper>
+          <StyledSnackIcon />
+          <h3>Snack</h3>
+        </MealTitleWrapper>
+        <NutririonList>
+          <li>
+            Carbonohidrates:<span></span>
+          </li>
+          <li>
+            Protein:<span></span>
+          </li>
+          <li>
+            Fat:<span></span>
+          </li>
+        </NutririonList>
+        <RecordMealWrapper>
+          <List>
+            <ListItem>
+              <p>1</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>2</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>3</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+            <ListItem>
+              <p>4</p>
+              <RecordBtn>
+                <RecordMealIcon />
+                <RecordBtnLabel>Record your meal</RecordBtnLabel>
+              </RecordBtn>
+            </ListItem>
+          </List>
+        </RecordMealWrapper>
+      </MealWrapper>
+    </DiarySection>
+  );
 }
--- src/pages/DiaryPage/DiaryPage.styled.js
+++ src/pages/DiaryPage/DiaryPage.styled.js
@@ -1,1 +1,89 @@
 import styled from 'styled-components';
+import { ReactComponent as BreakfastIcon } from '../../assets/images/diary/breackfast.svg';
+import { ReactComponent as GoBackIcon } from '../../assets/images/diary/goBack.svg';
+import { ReactComponent as LunchIcon } from '../../assets/images/diary/lunch.svg';
+import { ReactComponent as DinnerIcon } from '../../assets/images/diary/dinner.svg';
+import { ReactComponent as SnackIcon } from '../../assets/images/diary/snack.svg';
+import { ReactComponent as RecordIcon } from '../../assets/images/diary/recordMeal.svg';
+
+export const DiarySection = styled.section`
+  padding: 16px 10px 40px;
+`;
+
+export const GoBackWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 9px;
+  margin-bottom: 16px;
+`;
+
+export const GoBackBtn = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding: 0;
+`;
+
+export const StyledGoBackIcon = styled(GoBackIcon)`
+  width: 16px;
+  height: 16px;
+`;
+
+export const StyledBreakfastIcon = styled(BreakfastIcon)``;
+
+export const StyledLunchIcon = styled(LunchIcon)``;
+
+export const StyledDinnerIcon = styled(DinnerIcon)``;
+
+export const StyledSnackIcon = styled(SnackIcon)``;
+
+export const MealWrapper = styled.div``;
+
+export const MealTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const NutririonList = styled.ul`
+  display: flex;
+  gap: 20px;
+  width: 178px;
+  height: auto;
+  margin: 12px 0;
+`;
+
+export const NutririonListItem = styled.li``;
+
+export const RecordMealWrapper = styled.div``;
+
+export const List = styled.ul`
+  background-color: var(--black-secondary);
+  border-radius: 12px;
+  padding: 16px 12px;
+  margin-bottom: 24px;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+
+  &:not(:last-child) {
+    margin-bottom: 30px;
+  }
+`;
+
+export const RecordBtn = styled.button`
+  display: flex;
+  align-items: center;
+  margin-left: 16px;
+`;
+
+export const RecordMealIcon = styled(RecordIcon)`
+  margin-right: 6px;
+`;
+
+export const RecordBtnLabel = styled.label`
+  color: var(--green-light);
+  cursor: pointer;
+`;
