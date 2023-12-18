0559386375ec799792cf694ce4e9f8de31c1d5e2
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-12-19 00:34:56 +0200

    styled

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Container } from '../../components/StyledComponents/Container';
-
+import watchMob1x from '../../assets/images/watch/watch-mob@1x.png'
+import watchMob2x from '../../assets/images/watch/watch-mob@2x.png'
+import watchTab1x from '../../assets/images/watch/watch-tab@1x.png'
+import watchTab2x from '../../assets/images/watch/watch-tab@2x.png'
+import watchDesk1x from '../../assets/images/watch/watch-desk@1x.png'
+import watchDesk2x from '../../assets/images/watch/watch-desk@2x.png'
+import { Link } from 'react-router-dom';
+import { ListItem } from './WelcomePage.styled';
 export default function WelcomePage() {
   return (
     <main>
       <Container>
+        <img src={ watchMob1x } />
         <h1>Set goals and achieve them</h1>
         <h2>The service will help you set goals and follow them.</h2>
-
+        <Link to="/signin">
+        Sign in
+        </Link>
+        <Link to="signup">
+        Sign up
+        </Link>
         <ul>
-          <li>Set goals</li>
-          <li>Watch your calories</li>
-          <li>Keep track of your water intake</li>
-          <li>Control your weight</li>
+          <ListItem>Set goals</ListItem>
+          <ListItem>Watch your calories</ListItem>
+          <ListItem>Keep track of your water intake</ListItem>
+          <ListItem>Control your weight</ListItem>
         </ul>
       </Container>
     </main>
--- src/pages/WelcomePage/WelcomePage.styled.js
+++ src/pages/WelcomePage/WelcomePage.styled.js
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+export const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  &::before {
+    content: '';
+    border-radius: 50%;
+    width: 8px;
+    height: 8px;
+    background-color: #e3ffa8;
+  }
+`;
