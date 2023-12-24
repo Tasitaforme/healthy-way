6b8ff8a103cc5a9f65d12263573ba43afcfa7ed0
tasita <uatasita@gmail.com>
2023-12-24 18:17:11 +0200

    check for build (img header)

--- src/helpers/getRandomItems.js
+++ src/helpers/getRandomItems.js
@@ -1,4 +1,6 @@
 export const getRandomItems = (array, n) => {
-  const shuffled = array.slice().sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, n);
+  if (array) {
+    const shuffled = array.slice().sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, n);
+  }
 };
